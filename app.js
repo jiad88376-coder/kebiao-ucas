4f70eb564a0c3bce522ba1473ff1a3abe66f42e3
jiad88376-coder <jiad88376-coder@users.noreply.github.com>
2026-09-08 15:21:16 +0800

    feat: first patron in thanks list

--- app.js
+++ app.js
@@ -2110,6 +2110,7 @@ function esc(s) {
 }
 
 const THANKS = [
+  { name: "09081202", amount: "¥1", date: "2026-09-08" },
   // { name: "昵称", amount: "¥10", date: "2026-09-08", msg: "留言（可省略）" },
 ];
 
